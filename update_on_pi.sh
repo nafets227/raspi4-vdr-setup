006bbf62f6c88fc688b3a4dfc88b0f1b82bb37f9
Stefan Schallenberg <nafets227@users.noreply.github.com>
2021-06-20 22:27:28 +0200

    dont disable overscan

--- update_on_pi.sh
+++ update_on_pi.sh
@@ -7,13 +7,12 @@ function piwozi-updatesysconfig {
 	# we replace existing /boot/config.txt, default can be restored by deleting and
 	# reinstalling the package raspberrypi-bootloader
 	sudo bash -c "cat >/boot/config.txt" <<-EOF &&
-		# Version by Stefan Schallenberg install script
+		# Version from nafets227/raspi-by Stefan Schallenberg install script
 		#### comment dtoverlay=vc4-fkms-3d above
 		dtoverlay=vc4-kms-v3d-pi4,cma-512
 		dtoverlay=rpivid-v4l2
-		disable_overscan=1
 		disable_fw_kms_setup=1
-		# End Stefan Schallenberg 2.1.2021
+		# End Version from nafets227/raspi-by Stefan Schallenberg install script
 		EOF
 
 	sudo apt-get --yes install git || return 1
